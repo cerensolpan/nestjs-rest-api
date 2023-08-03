55c63a5dda61b8dff0b1d71a6f4fecebdc898aab
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-04 00:07:35 +0300

    feat: Create create book api

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
-
+import { CreateBookDto } from './dto/create-book.dto';
 @Controller('books') // url
 export class BookController {
-    constructor (private bookService: BookService){}
+    constructor(private bookService: BookService) { }
 
     @Get()
-    async getAllBooks(): Promise<Book[]>{
+    async getAllBooks(): Promise<Book[]> {
         return this.bookService.findAll();
     }
+
+    @Post()
+    async createBook(
+        @Body()
+        book: CreateBookDto
+    ): Promise<Book> {
+        return this.bookService.create(book);
+    }
+
+
 
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -8,10 +8,16 @@ export class BookService {
     constructor(
         @InjectModel(Book.name)
         private bookModel: mongoose.Model<Book>
-    ){}
+    ) { }
 
-    async findAll(): Promise<Book[]>{
-        const books = await this.bookModel.find();
+    async findAll(): Promise<Book[]> {
+        const books = await this.bookModel.find()
         return books;
     }
+
+    async create(book: Book): Promise<Book> {
+        const res = await this.bookModel.create(book);
+        return res;
+    }
+
 }
