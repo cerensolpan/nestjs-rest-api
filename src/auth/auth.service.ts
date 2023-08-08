58de82808604eee02e06d5cb1f2640ff9c3ed1c4
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-08 13:37:16 +0300

    feat: Write tests for auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
@@ -20,15 +20,22 @@ export class AuthService {
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
-        const user = await this.userModel.create({
-            name,
-            email,
-            password: hashedPassword
-        })
-
-        const token = this.jwtService.sign({ id: user._id })
-
-        return { token }
+        try {
+            const user = await this.userModel.create({
+                name,
+                email,
+                password: hashedPassword
+            })
+    
+            const token = this.jwtService.sign({ id: user._id })
+    
+            return { token }
+            
+        } catch (error) {
+            if(error?.code===11000){
+                throw new ConflictException('Duplicate Email Entered');
+            }
+        }
 
     }
 
